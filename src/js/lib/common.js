8e4595ae4dd5da647d9ba96a3a45432a659c4951
Furkan Taşkın <furkan.taskin@tuta.io>
2023-11-16 15:13:31 +0300

    Initial commit

---
+++ src/js/lib/common.js
@@ -0,0 +1,33 @@
+export function mobileMenu() {
+  let showMenu = false;
+  const mobileMenu = document.querySelector('.mobileMenu');
+  const mainNav = document.querySelector('.header_lg nav')
+  const mobileNav = mobileMenu.querySelector('.mobileMenu nav')
+  function toggleMenu(isActive) {
+    if (isActive) {
+      document
+        .querySelectorAll('body, .mobileMenu, .menuButton, html')
+        .forEach((e) => {
+          e.classList.add('show');
+        });
+    } else {
+      document
+        .querySelectorAll('body, .mobileMenu, .menuButton, html')
+        .forEach((e) => {
+          e.classList.remove('show');
+        });
+    }
+  }
+
+  document
+    .querySelector('.menuButton')
+    .addEventListener('click', () => {
+      showMenu = !showMenu;
+      toggleMenu(showMenu);
+    });
+
+  mobileNav.innerHTML = mainNav.innerHTML;
+
+  mobileMenu.style.paddingTop =
+    document.querySelector('.header_sm').clientHeight * 1.5 + 'px';
+}
